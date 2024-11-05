c6ba80cd028e234a6faf78033a499bb2ee998cb7
henriquemachado046 <henriquemachado046@yahoo.com>
2024-11-05 03:34:27 -0300

    Botão de mostrar/esconder gráfico está funcionalm tanto em professor quanto em aluno.

--- src/main/resources/static/js/charts_student.js
+++ src/main/resources/static/js/charts_student.js
@@ -1,41 +1,43 @@
-const xValues = [5,8,1,2,4,7,5,13,11,12,17];
-const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+const xValues = [5, 8, 1, 2, 4, 7, 5, 13, 11, 12, 17];
+const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
 //Aqui está um momento de escolha. Os gráficos serão definidos previamente ou poderão ser criados?
 
-function openChart() {
+//Dentro desta função deverá ser passado de parâmetro o que é necessário para criar os gráficos de maneira dinâmica.
+function createChart() {
+    if (document.getElementById("myChart") === null) {
+        const newCanvas = document.createElement("canvas", "myChart")
+        newCanvas.setAttribute("id", "myChart")
+        newCanvas.setAttribute("style", "width:100%;max-width:600px")
+        const position = document.getElementById("canvasDiv")
+        document.getElementById("canvasDiv").insertAdjacentElement("afterbegin", newCanvas)
+    }
 
-        if (document.getElementById("myChart") === null) {
-            const newCanvas = document.createElement("canvas", "myChart")
-            newCanvas.setAttribute("id", "myChart")
-            newCanvas.setAttribute("style","width:100%;max-width:600px")
-            const position = document.getElementById("canvasDiv")
-            document.getElementById("canvasDiv").insertAdjacentElement("afterbegin", newCanvas) 
-        }
-        
-        new Chart(document.getElementById('myChart').id, {
-            type: "line",
-            data: {
-              labels: xValues,
-              datasets: [{
+    new Chart(document.getElementById('myChart').id, {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{
                 fill: false,
                 lineTension: 0,
                 backgroundColor: "rgba(0,0,255,1.0)",
                 borderColor: "rgba(0,0,255,0.1)",
                 data: yValues
-              }]
-            },
-            options: {
-              legend: {display: false},
-              scales: {
-                yAxes: [{ticks: {min: 6, max:16}}],
-              }
+            }]
+        },
+        options: {
+            legend: { display: false },
+            scales: {
+                yAxes: [{ ticks: { min: 6, max: 16 } }],
             }
-          });
-
-        if (toggle.style.display === "none") {
-            toggle.style.display = "block";                
-        } else {
-            toggle.style.display = "none"                
         }
+    });
+}
+
+function showChart() {
+    const toggle = document.getElementById('myChart')
+    if (toggle.style.display === "none") {
+        toggle.style.display = "block";
+    } else {
+        toggle.style.display = "none"
     }
-   
+}
--- src/main/resources/static/js/charts_teacher.js
+++ src/main/resources/static/js/charts_teacher.js
@@ -1,44 +1,40 @@
-const xValues = [5,8,1,2,4,7,5,13,11,12,17];
-const yValues = [7,8,8,9,9,9,10,11,14,14,15];
-    function openChart() {
+const xValues = [5, 8, 1, 2, 4, 7, 5, 13, 11, 12, 17];
+const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+function createChart() {
 
-        if (document.getElementById("myChart") === null) {
-            const newCanvas = document.createElement("canvas", "myChart")
-            newCanvas.setAttribute("id", "myChart")
-            newCanvas.setAttribute("style","width:100%;max-width:600px")
-            const position = document.getElementById("canvasDiv")
-            document.getElementById("canvasDiv").insertAdjacentElement("afterbegin", newCanvas) 
-        }else{
-            const toggle = document.getElementById('myChart')
-
-            new Chart(document.getElementById('myChart').id, {
-                type: "line",
-                data: {
-                  labels: xValues,
-                  datasets: [{
-                    fill: false,
-                    lineTension: 0,
-                    backgroundColor: "rgba(0,0,255,1.0)",
-                    borderColor: "rgba(0,0,255,0.1)",
-                    data: yValues
-                  }]
-                },
-                options: {
-                  legend: {display: false},
-                  scales: {
-                    yAxes: [{ticks: {min: 6, max:16}}],
-                  }
-                }
-              });
-              //Conversar com o Igor, pois está meio bugado o botão. A ideia está correta.
-            if (toggle.style.display === "none") {
-                toggle.style.display = "block";                
-            } else {
-                toggle.style.display = "none"                
-            }
-            
-        }
-        
-       
+    if (document.getElementById("myChart") === null) {
+        const newCanvas = document.createElement("canvas", "myChart")
+        newCanvas.setAttribute("id", "myChart")
+        newCanvas.setAttribute("style", "width:100%;max-width:600px")
+        const position = document.getElementById("canvasDiv")
+        document.getElementById("canvasDiv").insertAdjacentElement("afterbegin", newCanvas)
     }
-   
+    new Chart(document.getElementById('myChart').id, {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{
+                fill: false,
+                lineTension: 0,
+                backgroundColor: "rgba(0,0,255,1.0)",
+                borderColor: "rgba(0,0,255,0.1)",
+                data: yValues
+            }]
+        },
+        options: {
+            legend: { display: false },
+            scales: {
+                yAxes: [{ ticks: { min: 6, max: 16 } }],
+            }
+        }
+    });
+}
+
+function showChart() {
+    const toggle = document.getElementById('myChart')
+    if (toggle.style.display === "none") {
+        toggle.style.display = "block";
+    } else {
+        toggle.style.display = "none"
+    }
+}  
