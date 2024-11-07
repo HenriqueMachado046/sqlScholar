2837d85612bfff337558314aed4d776871a9166f
henriquemachado046 <henriquemachado046@yahoo.com>
2024-11-07 01:45:17 -0300

    Gráficos inseridos. Metricas de professor e aluno com placeholders.

--- src/main/resources/static/js/charts_student.js
+++ src/main/resources/static/js/charts_student.js
@@ -1,15 +1,25 @@
+
 const xValues = [5, 8, 1, 2, 4, 7, 5, 13, 11, 12, 17];
 const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+const solved = 0;
 //Aqui está um momento de escolha. Os gráficos serão definidos previamente ou poderão ser criados?
+//Gráfico de questões acertadas por este aluno;
+//3 indicadores estatísticos para aluno e professores;
+//2 processos de gamificação;
+//1 gráfico pizza, 1 Gráfico Linha, 1 Gráfico barra; OK
+
+//Tabela
 
 //Dentro desta função deverá ser passado de parâmetro o que é necessário para criar os gráficos de maneira dinâmica.
 function createChart() {
     if (document.getElementById("myChart") === null) {
+        const newDiv = document.createElement("div", "chart1")
         const newCanvas = document.createElement("canvas", "myChart")
         newCanvas.setAttribute("id", "myChart")
         newCanvas.setAttribute("style", "width:100%;max-width:600px")
-        const position = document.getElementById("canvasDiv")
         document.getElementById("canvasDiv").insertAdjacentElement("afterbegin", newCanvas)
+        document.getElementById("chart1").insertAdjacentElement("afterbegin", newCanvas)
+
     }
 
     new Chart(document.getElementById('myChart').id, {
@@ -33,8 +43,91 @@ function createChart() {
     });
 }
 
+function createBarChart() {
+
+    if (document.getElementById("myChart2") === null) {
+        const newDiv = document.createElement("div", "chart2")
+        const newCanvas = document.createElement("canvas", "myChart2")
+        newCanvas.setAttribute("id", "myChart2")
+        newCanvas.setAttribute("style", "width:100%;max-width:600px")
+        newDiv.setAttribute("class", "float-child")
+        newDiv.setAttribute("id", "chart2")
+        document.getElementById("canvasDiv").insertAdjacentElement("afterbegin", newDiv)
+        document.getElementById("chart2").insertAdjacentElement("afterbegin", newCanvas)
+    }
+
+    new Chart(document.getElementById('myChart2'), {
+        type: 'bar',
+        data:{
+            labels: ['Janeiro', 'Fevereiro', 'Marco', 'Abril', 'Maio', 'Junho'],
+            datasets: [{
+                label: "Grafico barra do primeiro semestre do ano",
+                backgroundColor: 'rgb (255, 99, 126)',
+                borderColor: 'rgb (255, 99, 132)',
+                data: [15, 39, 24, 55, 12, 99]
+            }]
+        },
+        options:{
+            responsive: true,
+            legend:{
+                position: 'top',
+            },
+            title:{
+                display:true,
+                text: 'Exemplo barra'
+            }
+        }
+    })         
+};
+
+function createPieChart() {
+
+    const dataPie = {
+        labels: ['Area1', 'Area2', 'Area3', 'Area4'],
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: [12, 11, 22, 34],
+                backgroundColor: [
+                    '#36A2EB',
+                    'rgb (154, 162, 135)',
+                    'rgb (212, 99, 86)',
+                    'rgb (154, 232, 21)'
+                ],
+                hoverOffset: 4
+            }
+        ]
+    };
+
+    if (document.getElementById("myChart3") === null) {
+        const newDiv = document.createElement("div", "chart3")
+        const newCanvas = document.createElement("canvas", "myChart3")
+        newCanvas.setAttribute("id", "myChart3")
+        newCanvas.setAttribute("style", "width:100%;max-width:900px")
+        newDiv.setAttribute("class", "float-child")
+        newDiv.setAttribute("id", "chart3")
+        document.getElementById("canvasDiv").insertAdjacentElement("afterbegin", newDiv)
+        document.getElementById("chart3").insertAdjacentElement("afterbegin", newCanvas)
+    }
+
+    new Chart(document.getElementById('myChart3'), {
+        type: 'pie',
+        data: dataPie,
+        options:{
+            responsive: true,
+            legend:{
+                position: 'top',
+            },
+            title:{
+                display:true,
+                text: 'Exemplo pizza'
+            }
+        }
+    })
+};
+
 function showChart() {
-    const toggle = document.getElementById('myChart')
+    const toggle = document.getElementById('canvasDiv')
     if (toggle.style.display === "none") {
         toggle.style.display = "block";
     } else {
--- src/main/resources/static/js/charts_teacher.js
+++ src/main/resources/static/js/charts_teacher.js
@@ -1,19 +1,25 @@
 const xValues = [5, 8, 1, 2, 4, 7, 5, 13, 11, 12, 17];
 const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
-function createChart() {
 
+
+
+function createChart() {
     if (document.getElementById("myChart") === null) {
+        const newDiv = document.createElement("div", "chart1")
         const newCanvas = document.createElement("canvas", "myChart")
+        newDiv.setAttribute("id", "chart1")
+        newDiv.setAttribute("class", "float-child")
         newCanvas.setAttribute("id", "myChart")
         newCanvas.setAttribute("style", "width:100%;max-width:600px")
-        const position = document.getElementById("canvasDiv")
-        document.getElementById("canvasDiv").insertAdjacentElement("afterbegin", newCanvas)
+        document.getElementById("canvasDiv").insertAdjacentElement("afterbegin", newDiv)
+        document.getElementById("chart1").insertAdjacentElement("afterbegin", newCanvas)
     }
     new Chart(document.getElementById('myChart').id, {
         type: "line",
         data: {
             labels: xValues,
             datasets: [{
+                label: 'Grafico linha teste',
                 fill: false,
                 lineTension: 0,
                 backgroundColor: "rgba(0,0,255,1.0)",
@@ -22,16 +28,108 @@ function createChart() {
             }]
         },
         options: {
-            legend: { display: false },
+            options:{
+                responsive: true,
+                legend:{
+                    position: 'top',
+                },
+                title:{
+                    display:true,
+                    text: 'Exemplo linha'
+                }
+            },
             scales: {
                 yAxes: [{ ticks: { min: 6, max: 16 } }],
             }
         }
     });
-}
+};
+
+function createBarChart() {
+
+    if (document.getElementById("myChart2") === null) {
+        const newDiv = document.createElement("div", "chart2")
+        const newCanvas = document.createElement("canvas", "myChart2")
+        newCanvas.setAttribute("id", "myChart2")
+        newCanvas.setAttribute("style", "width:100%;max-width:600px")
+        newDiv.setAttribute("class", "float-child")
+        newDiv.setAttribute("id", "chart2")
+        document.getElementById("canvasDiv").insertAdjacentElement("afterbegin", newDiv)
+        document.getElementById("chart2").insertAdjacentElement("afterbegin", newCanvas)
+    }
+
+    new Chart(document.getElementById('myChart2'), {
+        type: 'bar',
+        data:{
+            labels: ['Janeiro', 'Fevereiro', 'Marco', 'Abril', 'Maio', 'Junho'],
+            datasets: [{
+                label: "Grafico barra do primeiro semestre do ano",
+                backgroundColor: 'rgb (255, 99, 126)',
+                borderColor: 'rgb (255, 99, 132)',
+                data: [15, 39, 24, 55, 12, 99]
+            }]
+        },
+        options:{
+            responsive: true,
+            legend:{
+                position: 'top',
+            },
+            title:{
+                display:true,
+                text: 'Exemplo barra'
+            }
+        }
+    })         
+};
+
+function createPieChart() {
+
+    const dataPie = {
+        labels: ['Area1', 'Area2', 'Area3', 'Area4'],
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: [12, 11, 22, 34],
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)',
+                    'rgb(115, 225, 19)'
+                ],
+                hoverOffset: 4
+            }
+        ]
+    };
+
+    if (document.getElementById("myChart3") === null) {
+        const newDiv = document.createElement("div", "chart3")
+        const newCanvas = document.createElement("canvas", "myChart3")
+        newCanvas.setAttribute("id", "myChart3")
+        newCanvas.setAttribute("style", "width:100%;max-width:900px")
+        newDiv.setAttribute("class", "float-child")
+        newDiv.setAttribute("id", "chart3")
+        document.getElementById("canvasDiv").insertAdjacentElement("afterbegin", newDiv)
+        document.getElementById("chart3").insertAdjacentElement("afterbegin", newCanvas)
+    }
+
+    new Chart(document.getElementById('myChart3'), {
+        type: 'pie',
+        data: dataPie,
+        options:{
+            responsive: true,
+            legend:{
+                position: 'top',
+            },
+            title:{
+                display:true,
+                text: 'Exemplo pizza'
+            }
+        }
+    })
+};
 
 function showChart() {
-    const toggle = document.getElementById('myChart')
+    const toggle = document.getElementById('canvasDiv')
     if (toggle.style.display === "none") {
         toggle.style.display = "block";
     } else {
