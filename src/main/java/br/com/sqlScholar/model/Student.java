d48d2c1ef310b86a85c093771f12209e9d7b78ac
henriquemachado046 <henriquemachado046@yahoo.com>
2024-12-11 04:04:10 -0300

    Criadas as colunas de acertos e erros em aluno.

--- src/main/java/br/com/sqlScholar/model/Student.java
+++ src/main/java/br/com/sqlScholar/model/Student.java
@@ -1,6 +1,7 @@
 package br.com.sqlScholar.model;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 
@@ -12,6 +13,12 @@ public class Student extends Person{
     @ManyToMany(mappedBy = "students")
     List<Teacher> teachers;
 
+    @Column
+    private int rightAnswers;
+
+    @Column
+    private int wrongAnswers;
+    
     public Student(){
         this.setTeacher(false);
     }
